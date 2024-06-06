992831483a919e64a2024ffcdbe912b528dbdeb7
yvcardenas <yvcardenas@ucsd.edu>
2024-06-05 18:06:07 -0700

    add documentation

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,13 +2,15 @@ document.addEventListener("DOMContentLoaded", function(){
     var video = document.getElementById("spaceVideo");
     var fallbackImage = document.getElementById("fallback-image");
 
+    // Check is the video can play
     video.oncanplay = function(){
-        fallbackImage.style.display = "none";
-        video.style.display = "block";
+        fallbackImage.style.display = "none"; // Hide fallback image
+        video.style.display = "block"; // Show video
     };
 
+    // Fallback if video fails to load
     video.onerror = function() {
-        fallbackImage.style.display = "block";
-        video.style.display = "none";
+        fallbackImage.style.display = "block"; // Show fallback image
+        video.style.display = "none"; // Hide video
     };
 });
