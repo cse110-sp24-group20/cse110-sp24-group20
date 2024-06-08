bee3941e5ad7f78b214aa2b2a3565f93d1635fec
roycehuang <130111798+roycehuang@users.noreply.github.com>
2024-06-08 12:44:24 -0700

    reverted change on test

--- src/javascript/project_tracker.js
+++ src/javascript/project_tracker.js
@@ -35,6 +35,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
     function createProjectElement(name) {
+        if (typeof name != 'string' || name === null){
+            console.error('Project name is undefined or null. Cannot create project');
+            return;
+        }
         const newProject = document.createElement('div'); // Create a new div element for the project
         newProject.className = 'project'; // Add the 'project' class to the div
         newProject.dataset.name = name; // Store the project name in a data attribute for later reference
